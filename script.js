661631d179ae3de2ad30e8047b2a512edc28a849
Atharva Gangras <acgangras@gmail.com>
2024-04-06 06:10:17 +0530

    add playGame function

--- script.js
+++ script.js
@@ -10,27 +10,52 @@ use while loop to run this game infinitely and give the user an option to quit
 2 - scissor
 */
 
-function getComputerChoice(){
-    let choice = Math.floor(Math.random()*3);
-    if(choice === 0) return "rock";
-    else if(choice ===1) return "paper";
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) return "rock";
+    else if (choice === 1) return "paper";
     return "scissors";
 }
-function getUserChoice(){
-    let choice = prompt("Enter Your Move:");
-    choice = choice.toLowerCase();
-    choice = choice.trim();
-    if(choice ==="rock"||choice==="paper"||choice==="scissors")
-    {
-        return choice;
+function getUserChoice() {
+    let choice;
+    while (true) {
+        choice = prompt("Enter Your Move:");
+        choice = choice.toLowerCase();
+        choice = choice.trim();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            return choice;
+        }
+        alert("Invalid Choice.Enter again");
     }
-    return "Invalid";
+
+}
+
+function playRound(computerChoice, userChoice) {
+
+    if (computerChoice === userChoice) return "Tie";
+    else if (computerChoice === "rock" && userChoice === "paper") return "user";
+    else if (computerChoice === "rock" && userChoice === "scissors") return "computer";
+    else if (computerChoice === "paper" && userChoice === "rock") return "computer";
+    else if (computerChoice === "paper" && userChoice === "scissors") return "user";
+    else if (computerChoice === "scissors" && userChoice === "rock") return "user";
+    else return "computer";
+}
+
+function playGame(){
+    let wins = 0;
+    for(let i = 0;i<5;i++)
+    {
+        let computerChoice = getComputerChoice();
+        let userChoice = getUserChoice();
+        let winner = playRound(computerChoice,userChoice);
+        if(winner === "user") wins++;
+    }
+    return wins;
+}
+
+while(true)
+{
     
+    playGame();
 }
 
-function playRound(computerChoice,userChoice){
-    if(userChoice === -1) return -1;
-
-    if(computerChoice === userChoice) return "Tie";
-    else if(computerChoice===2 && computerChoice===1)
-}
