79a4b4d355296e635c39bc12ee2b60ac4a4add9d
Atharva Gangras <acgangras@gmail.com>
2024-04-11 10:34:13 +0530

    add play again functionality

--- script.js
+++ script.js
@@ -16,15 +16,71 @@ function playRound(computerChoice, userChoice) {
 }
 
 const buttons = document.querySelectorAll('button');
-const div  = document.querySelector('div');
+const body = document.querySelector('body');
+const userChoiceUpdate = document.querySelector('#yourchoice')
+const computerChoiceUpdate = document.querySelector('#compchoice')
+
+const div = document.querySelector('#currentwinner');
+
+const userWinsUpdate = document.querySelector('#yourWinCount');
+const computerWinsUpdate = document.querySelector('#computerWinCount')
+
+let computerWins = 0;
+let yourWins = 0;
+const appendAns = document.createElement('div');
+
+function buttonClick(e)
+{
+    let userChoice = e.target.id;
+        let computerChoice = getComputerChoice();
+        userChoiceUpdate.textContent = userChoice;
+        computerChoiceUpdate.textContent = computerChoice;
+
+        let ans = playRound(computerChoice, userChoice)
+        div.textContent = ans;
+
+        if (ans === 'computer') ++computerWins;
+        else if (ans === 'user') ++yourWins;
+        userWinsUpdate.textContent = yourWins;
+        computerWinsUpdate.textContent = computerWins;
+
+        if (computerWins === 5) { 
+            appendAns.textContent = "The overall winner is Computer!"; 
+            body.appendChild(appendAns); 
+            setTimeout(playAgain,1);
+        }
+        else if (yourWins === 5) {
+            appendAns.textContent = "The overall winner is You!";
+            body.appendChild(appendAns);
+            
+            setTimeout(playAgain,1);
+        }
+}
 
 buttons.forEach((button) => {
-    button.addEventListener('click',(e)=>{
-        let userChoice = e.target.id;
-        let computerChoice = getComputerChoice();
-        
-        div.textContent =  playRound(computerChoice,userChoice);
-
-    })
+    button.addEventListener('click', buttonClick)
 })
+function playAgain(){
+    
+    let response = confirm("Do You want to play Again?");
+
+    if(response)
+    {
+        computerWins = 0;
+        yourWins = 0;
+        userWinsUpdate.textContent = yourWins;
+        computerWinsUpdate.textContent = computerWins;
+        userChoiceUpdate.textContent = '';
+        computerChoiceUpdate.textContent = '';
+        div.textContent = '';
+        appendAns.textContent = ""; 
+        body.appendChild(appendAns); 
+    }
+    else{
+        buttons.forEach((button)=>{
+            button.removeEventListener('click',buttonClick);
+        })
+    }
+}
+
 
